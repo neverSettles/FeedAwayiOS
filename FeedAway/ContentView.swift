702422fe8625bf2dee796050ab5b37ce9e3cbe6b
Christopher Settles <ChristopherRSettles@gmail.com>
2020-07-04 13:03:32 -0500

    Add video when not installed, need to replace with gif still

--- FeedAway/ContentView.swift
+++ FeedAway/ContentView.swift
@@ -8,7 +8,37 @@
 
 import SwiftUI
 import SafariServices
+import AVFoundation
 //import BlockerManager
+
+struct PlayerView: UIViewRepresentable {
+  func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+  }
+  func makeUIView(context: Context) -> UIView {
+    return PlayerUIView(frame: .zero)
+  }
+}
+
+class PlayerUIView: UIView {
+  private let playerLayer = AVPlayerLayer()
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    let url = URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!
+    let player = AVPlayer(url: url)
+    player.play()
+    
+    playerLayer.player = player
+    layer.addSublayer(playerLayer)
+  }
+  required init?(coder: NSCoder) {
+   fatalError("init(coder:) has not been implemented")
+  }
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    playerLayer.frame = bounds
+  }
+}
 
 struct ApplicationBlockerRow: View {
     let application: Application
@@ -62,7 +92,7 @@ struct ContentView: View {
             
             if !extensionActivatedObject.extensionActivated  {
                 Text("The FeedAway Extension is not activated on Safari Settings yet. Please activate.")
-                Image("FeedAwayInstall").resizable().frame(width: 32.0, height: 32.0)
+                PlayerView()
             }
             else {
                 Text("FeedAway Extension is activated! ")
